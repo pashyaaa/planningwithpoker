e94f04fe1b17a490c327119731022b4090044849
kalkaw84 <kalpakbkawadgave@gmail.com>
2023-11-08 00:34:41 +0530

    Some more Refactoring

--- src/app/poker/create-game/create-game.component.ts
+++ src/app/poker/create-game/create-game.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { addDoc, collection, doc, Firestore, updateDoc } from '@angular/fire/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PopoverController } from '@ionic/angular';
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-create-game',
@@ -16,44 +15,36 @@ export class CreateGameComponent implements OnInit {
   @Input() gameId: any;
   @Input() action: any;
 
-  form: FormGroup;
+  gameForm: FormGroup;
   saveClicked = false;
   numberSeries = [[0, 1, 2, 3, 5, 8, 13], [1, 2, 3, 4, 5]];
 
   constructor(
     private formBuilder: FormBuilder,
     private firestore: Firestore,
-  ) { }
+  ) {}
 
-  customSeriesValidator(required: boolean): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      if (required && (control.value === null || control.value === '')) {
-        return { required: true };
-      }
-      return null;
-    };
-  }
-
-  ngOnInit() {
+  async ngOnInit() {
     {
-      this.form = this.formBuilder.group({
+      this.gameForm = this.formBuilder.group({
         name: ['', [Validators.required]],
         numberSeries: ['', [Validators.required]],
         customSeries: ['', []], // No required validator initially
       });
 
       // Add a custom validator to 'customSeries' based on 'numberSeries' value
-      this.form.get('numberSeries').valueChanges.subscribe((value) => {
+      this.gameForm.get('numberSeries').valueChanges.subscribe((value) => {
         if (value === 'Custom') {
-          this.form.get('customSeries').setValidators([this.customSeriesValidator(true)]);
+          this.gameForm.get('customSeries').setValidators([Validators.required]);
         } else {
-          this.form.get('customSeries').setValidators([]);
+          this.gameForm.get('customSeries').setValidators([]);
         }
-        this.form.get('customSeries').updateValueAndValidity();
+        this.gameForm.get('customSeries').updateValueAndValidity();
       });
     }
   }
 
+  // Used to get Custom Number Series
   convertStringToArray(inputString: string): (number | string)[] {
     const inputArray = inputString.split(',');
     const resultArray: (number | string)[] = [];
@@ -68,43 +59,45 @@ export class CreateGameComponent implements OnInit {
     return resultArray;
   }
 
-  save() {
-    this.saveClicked = true;
-    console.log(this.form.get('name').value);
-
+  saveGame() {
     let tempNumberSeries;
-    if (this.form.get('numberSeries').value === 'Custom') {
-      tempNumberSeries = this.convertStringToArray(this.form.get('customSeries').value);
+
+    this.saveClicked = true;
+
+    if (this.gameForm.get('numberSeries').value === 'Custom') {
+      tempNumberSeries = this.convertStringToArray(this.gameForm.get('customSeries').value);
       this.numberSeries.push(tempNumberSeries);
     }
     else {
-      tempNumberSeries = this.numberSeries[this.form.get('numberSeries').value];
+      tempNumberSeries = this.numberSeries[this.gameForm.get('numberSeries').value];
     }
-    const game = {
-      name: this.form.get('name').value,
+    const updateGame = {
+      name: this.gameForm.get('name').value,
       numberSeries: tempNumberSeries
     };
 
     if (this.action === 'createGame') {
       const gamesRef = collection(this.firestore, 'games');
-
-      addDoc(gamesRef, game).then(
+      addDoc(gamesRef, updateGame).then(
         (game) => {
           this.saveClicked = false;
-          this.popoverController.dismiss({ gameId: game.id, selectedNumberSeries: this.numberSeries[this.form.get('numberSeries').value] });
+          this.popoverController.dismiss({
+            gameId: game.id,
+            selectedNumberSeries: this.numberSeries[this.gameForm.get('numberSeries').value]
+          });
         }
-      )
-    } else {
+      );
+    }
+    else {
       const gamesRef = doc(this.firestore, 'games/' + this.gameId);
-      updateDoc(gamesRef, game).then(
-        (player) => {
+      updateDoc(gamesRef, updateGame).then(
+        () => {
           this.saveClicked = false;
-          this.popoverController.dismiss({ gameId: this.gameId });
+          this.popoverController.dismiss({
+            gameId: this.gameId
+          });
         }
-      )
+      );
     }
   }
-
 }
-
-
