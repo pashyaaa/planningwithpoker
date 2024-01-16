c2455d6ad2d1c999b36cfc7a204dc7019bf41bb7
Ganesh Chincholkar <ganesh.chincholkar@siemens.com>
2024-01-16 18:56:15 +0530

    Added firebase config

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebase = {
+  apiKey: 'AIzaSyCrt5nlKVOqZkGwA-YoqMzi5EHeNc0Dv6s',
+  authDomain: 'test-firestore-c8cf0.firebaseapp.com',
+  projectId: 'test-firestore-c8cf0',
+  storageBucket: 'test-firestore-c8cf0.appspot.com',
+  messagingSenderId: '499027369540',
+  appId: '1:499027369540:web:62a6462422e4fea0fea611',
+};
+
+export default firebase;
