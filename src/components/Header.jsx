8c1efdac94701cafe03eb2461db3f930733c4b54
Ganesh Chincholkar <ganesh.chincholkar@siemens.com>
2024-01-18 20:02:17 +0530

    Seperate user component in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,63 +1,20 @@
-import { useState } from 'react';
-
 import {
   AppBar,
   Toolbar,
   Typography,
-  Button,
-  IconButton,
-  Menu,
-  MenuItem,
 } from '@mui/material';
 
-import { AccountCircle } from '@mui/icons-material';
+import User from './User';
 
 const Header = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  
   return (
     <AppBar>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Planning With Poker
         </Typography>
-        <div>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleMenu}
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My account</MenuItem>
-          </Menu>
-        </div>
+        <User></User>
       </Toolbar>
     </AppBar>
   );
---
+++ src/components/User.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+
+import { AccountCircle } from '@mui/icons-material';
+
+const User = () => {
+const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+        <IconButton
+        size="large"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleMenu}
+        color="inherit"
+        >
+        <AccountCircle />
+        </IconButton>
+        <Menu
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+        }}
+        keepMounted
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        </Menu>
+    </div>
+  )
+}
+
+export default User
