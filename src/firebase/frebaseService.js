520461240fd787ab9e6e470b25457865d2acad25
Ganesh Chincholkar <ganesh.chincholkar@siemens.com>
2024-01-22 19:36:20 +0530

    Firebase for game

--- src/firebase/frebaseService.js
+++ src/firebase/frebaseService.js
@@ -12,34 +12,86 @@ import firebaseConfig from './firebaseConfig';
 initializeApp(firebaseConfig);
 
 const db = getFirestore();
-const userCollection = collection(db, 'users');
+const usersCollection = collection(db, 'users');
+const gamesCollection = collection(db, 'games');
 
 export const getUser = async (userId) => {
-  if (userId === null) {
+  if (!userId) {
     throw new Error('Invalid userId');
   }
 
   try {
     const userRef = doc(db, 'users', userId);
-    const user = await getDoc(userRef);
-    if (user.exists()) {
-      return { id: user.id, ...user.data() };
+    const userDoc = await getDoc(userRef);
+    if (userDoc.exists()) {
+      return { id: userDoc.id, ...userDoc.data() };
     }
     return null;
   } catch (e) {
     console.error(e);
-    throw new Error('Error getting user from firestore');
+    throw new Error('Error fetching user from firestore');
   }
 };
 
 export const createUser = async (user) => {
+  if (!user || !user.name) {
+    throw new Error('Invalid user');
+  }
   try {
-    const userRef = await addDoc(userCollection, {
+    const userRef = await addDoc(usersCollection, {
       name: user.name,
     });
-    return {...user, id: userRef.id}
+    return { ...user, id: userRef.id };
   } catch (e) {
     console.error(e);
-    throw new Error('Error creating user');
+    throw new Error('Error creating user in firestore');
+  }
+};
+
+/**
+ *
+ * @param {Object} game
+ * @returns created game
+ */
+export const createGame = async (game) => {
+  if (!game || !game.name) {
+    throw new Error('Invalid game name');
+  }
+  if (!game || !game.createdBy) {
+    throw new Error('Invalid createdBy field');
+  }
+  if (!game || !game.cards) {
+    throw new Error('Invalid cards');
+  }
+
+  try {
+    const finalGame = {
+      name: game.name,
+      createdBy: game.createdBy,
+      cards: game.cards,
+    };
+    const gameRef = await addDoc(gamesCollection, finalGame);
+    return { id: gameRef.id, ...finalGame };
+  } catch (e) {
+    console.error(e);
+    throw new Error('Error creating game in firestore');
+  }
+};
+
+export const getGame = async (gameId) => {
+  if (!gameId) {
+    throw new Error('Invalid game id');
+  }
+
+  try {
+    const gameRef = doc(db, 'games', gameId);
+    const gameDoc = await getDoc(gameRef);
+    if (gameDoc.exists()) {
+      return { id: gameDoc.id, ...gameDoc.data() };
+    }
+    return null;
+  } catch (e) {
+    console.error(e);
+    throw new Error('Error fetching game from firestore');
   }
 };
