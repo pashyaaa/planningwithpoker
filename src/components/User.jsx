fe9dfa36c3224197495d065112bdedae51ae5822
Ganesh Chincholkar <gnesh.pc@gmail.com>
2024-02-10 01:07:22 +0530

    user menu added

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,20 @@
 import { useState } from 'react';
 
-import { IconButton, Menu, MenuItem } from '@mui/material';
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+  Avatar,
+  ListItemIcon,
+  Divider,
+} from '@mui/material';
 
 import { AccountCircle } from '@mui/icons-material';
+
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 
 const User = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -13,6 +25,11 @@ const User = () => {
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
   return (
@@ -27,24 +44,61 @@ const User = () => {
       >
         <AccountCircle />
       </IconButton>
-      {/* <Menu
-        id="menu-appbar"
+      <Menu
         anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={Boolean(anchorEl)}
+        open={open}
         onClose={handleClose}
+        onClick={handleClose}
+        elevation={0}
+        sx={{
+          overflow: 'visible',
+          filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+          mt: 1.5,
+          '& .MuiAvatar-root': {
+            width: 32,
+            height: 32,
+            ml: -0.5,
+            mr: 1,
+          },
+          '&::before': {
+            content: '""',
+            display: 'block',
+            position: 'absolute',
+            top: '3.5rem',
+            right: '2.5rem',
+            width: 10,
+            height: 10,
+            bgcolor: 'background.paper',
+            transform: 'translateY(-50%) rotate(45deg)',
+            zIndex: 0,
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-      </Menu> */}
+        <MenuItem >
+          <Avatar /> Profile
+          <ListItemIcon>
+            <ModeEditOutlineOutlinedIcon fontSize="small" sx={{marginLeft: '2rem'}}  />
+          </ListItemIcon>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Leave Game
+        </MenuItem>
+      </Menu>
     </div>
   );
 };
