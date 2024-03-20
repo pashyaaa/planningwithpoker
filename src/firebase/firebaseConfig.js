b9eb2daa423af0a0a9180e1cf116124d149be38c
Ganesh Chincholkar <ganesh.chincholkar@siemens.com>
2024-03-20 17:03:55 +0530

    env file added

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebase = {
-  apiKey: 'AIzaSyCrt5nlKVOqZkGwA-YoqMzi5EHeNc0Dv6s',
-  authDomain: 'test-firestore-c8cf0.firebaseapp.com',
-  projectId: 'test-firestore-c8cf0',
-  storageBucket: 'test-firestore-c8cf0.appspot.com',
-  messagingSenderId: '499027369540',
-  appId: '1:499027369540:web:62a6462422e4fea0fea611',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebase;
